ccceba7e9553fd17ce1b421359d06b168fc3a32d
martinpiskacek9 <martin.piskacek9@gmail.com>
2025-11-20 20:01:11 +0100

    vytvoření Navbar komponenty

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+const Navbar = () => {
+  return (
+    <nav>
+        <a href="/">Martin Piskáček</a>
+        <ul>
+            <li><a href="/">home</a></li>
+            <li><a href="#galleries">galleries</a></li>
+            <li><a href="#about">about me</a></li>
+            <li><a href="#contact">contact</a></li>
+        </ul>
+    </nav>
+  )
+}
+
+export default Navbar
