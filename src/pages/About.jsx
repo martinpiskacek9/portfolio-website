766fffd0e1756a81d62175668f08cd6d9ac58a79
martinpiskacek9 <martin.piskacek9@gmail.com>
2025-12-08 20:02:32 +0100

    Přidání obsahu do about sekce - nadpis, text, obrázek

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,9 +2,23 @@ const About = () => {
   return (
     <section
     id="about"
-    className=""
+    className="w-full items-stretch flex justify-center py-26 px-52 gap-16"
     >
-      
+      <div className="flex-3">
+        <h2>Get to know me</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore sit ipsam hic quae maiores alias id quisquam incidunt ullam ad sequi non, qui inventore corrupti recusandae debitis mollitia repellendus perspiciatis.
+        Distinctio a recusandae fugit voluptate, eligendi magnam, suscipit architecto non ex blanditiis molestias soluta. Ipsum in fuga voluptatem minus dolore natus, fugiat facilis esse officiis aperiam commodi voluptas atque illo?
+        Illo vero minima optio laudantium odio fugit esse rerum! Excepturi, nostrum necessitatibus. Laboriosam pariatur molestiae deleniti. Laudantium, nihil in sit consectetur, blanditiis aut enim illum ipsam tempora architecto id dolore.<br /><br /> Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas voluptate et ex consequatur autem, placeat non! Cum quibusdam omnis minus adipisci, temporibus sit perspiciatis corrupti repellat delectus ut dolor laborum?
+        Ratione perspiciatis, dicta ea praesentium aperiam culpa. Nulla voluptatum ab reprehenderit quisquam libero amet molestiae quibusdam possimus adipisci dolorem culpa quia voluptatibus laudantium placeat explicabo, expedita deserunt nam tempore excepturi?
+        Enim et qui debitis deleniti, animi nisi, autem, impedit corporis labore eaque officia. Iure modi unde porro, est natus doloremque, deserunt, maiores quasi architecto tenetur rerum praesentium saepe officia tempore.
+        Voluptatibus voluptatum esse nobis inventore dolore, numquam, impedit eius labore ab laboriosam, perspiciatis velit adipisci cupiditate voluptatem possimus facilis maiores illo assumenda nihil magnam autem repudiandae aliquam vel. Animi, vel.
+        Assumenda voluptatibus magni saepe dolorem reprehenderit numquam, quaerat officia. Labore voluptatem quia libero amet corrupti dolore quae sint cupiditate itaque perferendis aut, vel vero error hic laborum similique quis aperiam! </p>
+      </div>
+
+      <div 
+      className="flex-2 bg-center bg-cover bg-no-repeat"
+      style={{ backgroundImage: "url(imgs/_DSC9937.webp)" }}
+      />
     </section>
   )
 }
