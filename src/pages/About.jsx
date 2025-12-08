32d517730388ea1c3ae82e9d9f0d04adb0f4000a
martinpiskacek9 <martin.piskacek9@gmail.com>
2025-12-08 20:36:40 +0100

    Stylování about sekce, poladění responzivity

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,23 +2,23 @@ const About = () => {
   return (
     <section
     id="about"
-    className="w-full items-stretch flex justify-center py-26 px-52 gap-16"
+    className="w-full flex justify-center items-center py-26 px-6 md:px-26 2xl:px-52"
     >
-      <div className="flex-3">
-        <h2>Get to know me</h2>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore sit ipsam hic quae maiores alias id quisquam incidunt ullam ad sequi non, qui inventore corrupti recusandae debitis mollitia repellendus perspiciatis.
-        Distinctio a recusandae fugit voluptate, eligendi magnam, suscipit architecto non ex blanditiis molestias soluta. Ipsum in fuga voluptatem minus dolore natus, fugiat facilis esse officiis aperiam commodi voluptas atque illo?
-        Illo vero minima optio laudantium odio fugit esse rerum! Excepturi, nostrum necessitatibus. Laboriosam pariatur molestiae deleniti. Laudantium, nihil in sit consectetur, blanditiis aut enim illum ipsam tempora architecto id dolore.<br /><br /> Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas voluptate et ex consequatur autem, placeat non! Cum quibusdam omnis minus adipisci, temporibus sit perspiciatis corrupti repellat delectus ut dolor laborum?
-        Ratione perspiciatis, dicta ea praesentium aperiam culpa. Nulla voluptatum ab reprehenderit quisquam libero amet molestiae quibusdam possimus adipisci dolorem culpa quia voluptatibus laudantium placeat explicabo, expedita deserunt nam tempore excepturi?
-        Enim et qui debitis deleniti, animi nisi, autem, impedit corporis labore eaque officia. Iure modi unde porro, est natus doloremque, deserunt, maiores quasi architecto tenetur rerum praesentium saepe officia tempore.
-        Voluptatibus voluptatum esse nobis inventore dolore, numquam, impedit eius labore ab laboriosam, perspiciatis velit adipisci cupiditate voluptatem possimus facilis maiores illo assumenda nihil magnam autem repudiandae aliquam vel. Animi, vel.
-        Assumenda voluptatibus magni saepe dolorem reprehenderit numquam, quaerat officia. Labore voluptatem quia libero amet corrupti dolore quae sint cupiditate itaque perferendis aut, vel vero error hic laborum similique quis aperiam! </p>
+      <div className="w-full h-full flex justify-between items-stretch gap-18 flex-col lg:flex-row">
+        <div className="flex-3 xl:flex-7 h-full">
+          <h2 className="text-center md:text-left">Get to know me</h2>
+          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ducimus voluptate excepturi officiis deleniti impedit suscipit minima, labore aut, obcaecati provident autem libero totam aspernatur eos repudiandae, possimus mollitia consequatur dolorem.
+          Dolorum natus provident illum aliquam! Nostrum illo sunt non laboriosam? Magnam nobis natus tempora aut unde expedita dignissimos aperiam dolorum et dolor voluptatem neque iure hic, voluptatum nemo ipsum quaerat?
+          Culpa fugiat, doloremque accusantium temporibus autem nesciunt eos quas distinctio? Tenetur excepturi officiis suscipit sint eum inventore a voluptates necessitatibus quaerat, eveniet odio voluptate sed placeat voluptatum, quod beatae temporibus. <br /><br />Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorem consectetur doloribus maxime dolores recusandae hic magnam eius ducimus, ad iure odit a numquam dolorum quasi nemo inventore non provident asperiores?
+          A vitae quisquam nisi maiores. Earum molestiae repellat a placeat nesciunt consectetur doloribus fugiat dolorem nam eaque asperiores, eos quo eveniet deserunt accusamus nostrum, recusandae ullam, magnam voluptatem officiis ex.
+          Ad atque laboriosam temporibus exercitationem, ipsam reprehenderit! Fugit et voluptate non, in assumenda velit necessitatibus sequi soluta deleniti dolorum, eligendi molestiae itaque. Sequi quisquam possimus consectetur officia velit ipsa explicabo?
+          Corporis ab labore, expedita possimus exercitationem reiciendis hic dolore, veniam pariatur laudantium temporibus et? Et ipsam impedit iure dolorem incidunt odio fugit consequuntur aperiam facere eius earum nobis, perferendis quaerat. </p>
+        </div>
+        <div
+        className="flex-2 xl:flex-3 bg-bottom bg-cover bg-no-repeat hidden lg:block"
+        style={{ backgroundImage: "url(imgs/_DSC9937.webp)" }}
+        />
       </div>
-
-      <div 
-      className="flex-2 bg-center bg-cover bg-no-repeat"
-      style={{ backgroundImage: "url(imgs/_DSC9937.webp)" }}
-      />
     </section>
   )
 }
