286edcdef8bdb95be52844c0a069cf5f39f2dea6
martinpiskacek9 <martin.piskacek9@gmail.com>
2025-12-08 22:13:00 +0100

    naplnění image slideru, animace v index.css

--- image-slider-config.jsx
+++ image-slider-config.jsx
@@ -9,35 +9,27 @@ import img8 from "./src/imgs/slider/_DSC9997.webp"
 
 export const imageSlider = [
     {
-        id: 1,
         image: img1
     },
     {
-        id: 2,
         image: img2
     },
     {
-        id: 3,
         image: img3
     },
     {
-        id: 4,
         image: img4
     },
     {
-        id: 5,
         image: img5
     },
     {
-        id: 6,
         image: img6
     },
     {
-        id: 7,
         image: img7
     },
     {
-        id: 8,
         image: img8
     },
 ]
