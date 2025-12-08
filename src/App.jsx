b95b64ff42d8b12b9501858afea9c9e1c743c685
martinpiskacek9 <martin.piskacek9@gmail.com>
2025-12-08 21:17:51 +0100

    Page divider odstraněn

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import About from "./pages/About";
 import Navbar from "./components/Navbar";
-import PageDivider from "./components/PageDivider";
 import Galleries from "./pages/Galleries";
 import Hero from "./pages/Hero"
 
