c6b125b5bdb088a3843177b900bd1bef79f12110
martinpiskacek9 <martin.piskacek9@gmail.com>
2025-12-08 22:44:01 +0100

    úprava komponenty galleryCategory - smazání hover:scale-95

--- src/components/GalleryCategory.jsx
+++ src/components/GalleryCategory.jsx
@@ -6,7 +6,7 @@ const GalleryCategory = ({ heading, img }) => {
       className="w-full h-full flex bg-center bg-no-repeat bg-cover hover:scale-105 duration-300 hover:duration-300 outline-neutral-300 hover:outline"
       style={{ backgroundImage: `linear-gradient(rgba(0,0,0,0.2), rgba(0,0,0,0.2)), url(${img})` }}
       >
-        <a href="#" className="font-montserrat w-full h-full flex items-center justify-center md:opacity-0 opacity-100 text-2xl md:text-xl uppercase font-bold hover:opacity-100 bg-black/50 hover:bg-black/80 hover:text-neutral-50 hover:scale-95 duration-300"
+        <a href="#" className="font-montserrat w-full h-full flex items-center justify-center md:opacity-0 opacity-100 text-2xl md:text-xl uppercase font-bold hover:opacity-100 bg-black/50 hover:bg-black/80 hover:text-neutral-50 duration-300"
         >
           {heading}
         </a>
