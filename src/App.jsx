7668303793d3d56c2d7ca78e6e31bfdc613df449
martinpiskacek9 <martin.piskacek9@gmail.com>
2025-12-08 22:31:27 +0100

    Odstranění image slideru a configu, nehodí se do stránky

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import About from "./pages/About";
 import Navbar from "./components/Navbar";
 import Galleries from "./pages/Galleries";
 import Hero from "./pages/Hero"
-import ImageSlider from "./components/ImageSlider";
 
 const App = () => {
   return (
@@ -14,7 +13,6 @@ const App = () => {
     <main>
       <Hero />
       <Galleries />
-      <ImageSlider />
       <About />
     </main>
     </>
