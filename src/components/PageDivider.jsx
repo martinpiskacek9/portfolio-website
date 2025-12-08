10de5d7d65bf0718d3be3db4b5e762ba4c65ba75
martinpiskacek9 <martin.piskacek9@gmail.com>
2025-12-08 23:08:04 +0100

    Vytvoření secondary-color, použití v PageDivideru, dokončení PageDivideru

--- src/components/PageDivider.jsx
+++ src/components/PageDivider.jsx
@@ -1,8 +1,16 @@
 const PageDivider = () => {
   return (
-    <div>
-      
-    </div>
+    <section>
+        <div
+        className="relative w-full h-180 bg-cover bg-no-repeat flex items-center justify-center font-black font-montserrat uppercase"
+        style={{ backgroundImage: `radial-gradient(rgba(0,0,0,0.2), rgba(0,0,0,1)), url("imgs/_DSC3987-HDR-4.webp")` }}
+        >
+            <h3 className="text-6xl">Enjoying the <span className="secondary-color italic">light</span></h3>
+            <div className="absolute w-full h-full bg-linear-to-b from-black via-transparent to-black top-0" />
+
+        </div>
+    </section>
+    
   )
 }
 
