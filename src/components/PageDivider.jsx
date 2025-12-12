f9ac9587bbf77c00677154f147e27a04caa98d54
martinpiskacek9 <martin.piskacek9@gmail.com>
2025-12-13 00:58:33 +0100

    Úprava page divideru, změnu bg img, odstranění textu

--- src/components/PageDivider.jsx
+++ src/components/PageDivider.jsx
@@ -2,12 +2,9 @@ const PageDivider = () => {
   return (
     <section>
         <div
-        className="relative w-full h-180 bg-cover bg-no-repeat flex items-center justify-center font-black font-montserrat uppercase"
-        style={{ backgroundImage: `radial-gradient(rgba(0,0,0,0.2), rgba(0,0,0,1)), url("imgs/_DSC5754.webp")` }}
-        >
-            <div className="absolute w-full h-full bg-linear-to-b from-black via-transparent to-black top-0" />
-
-        </div>
+        className="w-full h-120 bg-cover bg-center bg-no-repeat"
+        style={{ backgroundImage: `linear-gradient(rgba(0,0,0,0.1), rgba(0,0,0,0.9)), url("imgs/_DSC5754.webp")` }}
+        />
     </section>
     
   )
