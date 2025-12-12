98189b8790df96709f79e72aebba1385587bce07
martinpiskacek9 <martin.piskacek9@gmail.com>
2025-12-12 22:34:41 +0100

    Vytvoření formu, stylování

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -2,9 +2,27 @@ const Contact = () => {
   return (
     <section
     id="contact"
-    className=""
+    className="w-full h-full flex flex-col items-center justify-start py-26 bg-neutral-500"
     >
+        <h2>Get in touch</h2>
+        <form className="flex flex-col items-center justify-center gap-8 w-[60vw] h-full">
+            <div className="flex items-center gap-4 w-full">
+                <div className="flex flex-col gap-2 flex-1">
+                    <label htmlFor="form_name">Name</label>
+                    <input id="form_name" type="text" />
+                </div>
+                <div className="flex flex-col gap-2 flex-1">
+                    <label htmlFor="form_email">Email</label>
+                    <input id="form_email" type="email" />
+                </div>
+            </div>
 
+            <div className="flex flex-col gap-2 w-full">
+                <label htmlFor="form_message">Message</label>
+                <textarea id="form_message" className="min-h-40" />
+            </div>
+            <input type="submit" value="Send" />
+        </form>
     </section>
   )
 }
