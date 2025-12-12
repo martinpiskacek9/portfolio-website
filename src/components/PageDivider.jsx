47d3f4ec6b225c8c3de941dc76664af8542e47b5
martinpiskacek9 <martin.piskacek9@gmail.com>
2025-12-13 00:52:36 +0100

    PageDivider zase nahozen

--- src/components/PageDivider.jsx
+++ src/components/PageDivider.jsx
@@ -3,9 +3,8 @@ const PageDivider = () => {
     <section>
         <div
         className="relative w-full h-180 bg-cover bg-no-repeat flex items-center justify-center font-black font-montserrat uppercase"
-        style={{ backgroundImage: `radial-gradient(rgba(0,0,0,0.2), rgba(0,0,0,1)), url("imgs/_DSC3987-HDR-4.webp")` }}
+        style={{ backgroundImage: `radial-gradient(rgba(0,0,0,0.2), rgba(0,0,0,1)), url("imgs/_DSC5754.webp")` }}
         >
-            <h3 className="text-6xl tracking-wide">Enjoy the <span className="secondary-color italic">light</span></h3>
             <div className="absolute w-full h-full bg-linear-to-b from-black via-transparent to-black top-0" />
 
         </div>
