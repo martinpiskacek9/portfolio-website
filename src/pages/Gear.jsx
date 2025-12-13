724a4b3d77c11b7b0bb63718d9c6456dc3b0f61d
martinpiskacek9 <martin.piskacek9@gmail.com>
2025-12-13 23:30:03 +0100

    vytvoření komponenty GearHeading, import do Gear stránky

--- src/pages/Gear.jsx
+++ src/pages/Gear.jsx
@@ -1,15 +1,17 @@
+import GearHeading from "../components/GearHeading";
+
 const Gear = () => {
   return (
     <section className="w-full h-full">
         <div
-        className="w-full h-100 flex items-center justify-center flex-col bg-cover bg-bottom bg-no-repeat"
+        className="w-full h-100 flex items-center justify-center flex-col bg-cover bg-top bg-no-repeat"
         style={{ backgroundImage: "linear-gradient(rgba(0,0,0,0.6), rgba(0,0,0,0.4)), url(imgs/_DSC8863.webp)" }}
         >
           <h1 className="text-5xl font-montserrat font-black tracking-wide italic">MY GEAR</h1>
         </div>
 
-        <div>
-
+        <div className="w-full h-full py-26 px-52">
+          <GearHeading />
         </div>
     </section>
   )
