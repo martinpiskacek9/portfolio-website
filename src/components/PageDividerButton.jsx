3dcc48c1ad68676fae14e6dafa0ab42721efcefa
martinpiskacek9 <martin.piskacek9@gmail.com>
2025-12-13 05:14:20 +0100

    Poladění responzivity na PageDivideru

--- src/components/PageDividerButton.jsx
+++ src/components/PageDividerButton.jsx
@@ -4,13 +4,13 @@ import { faArrowRightLong } from '@fortawesome/free-solid-svg-icons'
 
 const PageDividerButton = ({ btnLink, btnHeading, btnText, side }) => {
   return (
-    <div className={`w-full h-full bg-neutral-950/30 hover:bg-neutral-950/5 duration-300 ${ side === "left" ? "pl-75" : "pr-75" }`}>
+    <div className={"w-full h-full flex bg-neutral-950/30 hover:bg-neutral-950/5 duration-300"}>
       <a
       href={btnLink}
-      className="divider-btn h-full flex items-center justify-center flex-col text-4xl font-bebas tracking-widest font-bold uppercase"
+      className={`divider-btn w-full h-full py-26 sm:py-0 flex items-center justify-center flex-col text-3xl lg:text-4xl font-bebas tracking-widest font-bold uppercase ${ side === "left" ? "pl-0 xl:pl-35 2xl:pl-75" : "pr-0 xl:pr-35 2xl:pr-75" }`}
       >
         {btnHeading}
-        <p className="text-xs font-montserrat mt-4 mb-8 font-light tracking-widest">
+        <p className="text-xs font-montserrat mt-2 mb-6 lg:mt-4 lg:mb-8 font-light tracking-widest duration-300">
             {btnText}
         </p>
         <FontAwesomeIcon className="text-base" icon={faArrowRightLong} />
