5b6d46e52116e67d21795a6322f752feb295d5be
martinpiskacek9 <martin.piskacek9@gmail.com>
2025-12-13 23:24:10 +0100

    stylování gear-page

--- src/pages/Gear.jsx
+++ src/pages/Gear.jsx
@@ -1,7 +1,16 @@
 const Gear = () => {
   return (
-    <section className="w-full h-screen flex items-center justify-center">
-        <h2>MY GEAR</h2>
+    <section className="w-full h-full">
+        <div
+        className="w-full h-100 flex items-center justify-center flex-col bg-cover bg-bottom bg-no-repeat"
+        style={{ backgroundImage: "linear-gradient(rgba(0,0,0,0.6), rgba(0,0,0,0.4)), url(imgs/_DSC8863.webp)" }}
+        >
+          <h1 className="text-5xl font-montserrat font-black tracking-wide italic">MY GEAR</h1>
+        </div>
+
+        <div>
+
+        </div>
     </section>
   )
 }
