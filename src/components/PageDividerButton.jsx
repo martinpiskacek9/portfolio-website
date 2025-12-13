f407b29293bac6d43c1159e835f5d1ff1daff7b7
martinpiskacek9 <martin.piskacek9@gmail.com>
2025-12-13 22:57:24 +0100

    v page divider použití Link místo anchor tagu

--- src/components/PageDividerButton.jsx
+++ src/components/PageDividerButton.jsx
@@ -1,12 +1,12 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faArrowRightLong } from '@fortawesome/free-solid-svg-icons'
-
+import { Link } from "react-router-dom";
 
 const PageDividerButton = ({ btnLink, btnHeading, btnText, side }) => {
   return (
     <div className={"w-full h-full flex bg-neutral-950/30 hover:bg-neutral-950/5 duration-300"}>
-      <a
-      href={btnLink}
+      <Link
+      to={btnLink}
       className={`divider-btn w-full h-full py-26 sm:py-0 flex items-center justify-center flex-col text-3xl lg:text-4xl font-bebas tracking-widest font-bold uppercase ${ side === "left" ? "pl-0 xl:pl-35 2xl:pl-75" : "pr-0 xl:pr-35 2xl:pr-75" }`}
       >
         {btnHeading}
@@ -14,7 +14,7 @@ const PageDividerButton = ({ btnLink, btnHeading, btnText, side }) => {
             {btnText}
         </p>
         <FontAwesomeIcon className="text-base" icon={faArrowRightLong} />
-      </a>
+      </Link>
     </div>
   )
 }
