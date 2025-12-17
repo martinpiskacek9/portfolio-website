2be1f6e1dcd565c616105960ed1721d585609869
martinpiskacek9 <martin.piskacek9@gmail.com>
2025-12-17 17:47:29 +0100

    Vytvoření cfg souboru pro gear itemy

---
+++ gear-config.jsx
@@ -0,0 +1,45 @@
+import imgCamera from "./src/imgs/gear/A7III.webp";
+import imgLens2470 from "./src/imgs/gear/samyang_24-70mm.webp";
+import imgLens14 from "./src/imgs/gear/sony_14mm.webp";
+import imgLens80 from "./src/imgs/gear/sony_85mm.webp";
+import imgLens70200 from "./src/imgs/gear/sony_70-200mm.webp";
+import imgTripod from "./src/imgs/gear/benro_tripod.webp";
+import imgBag from "./src/imgs/gear/shimoda.webp";
+
+export const gear = [
+    {
+        id: 1,
+        heading: "Sony a7 III",
+        image: imgCamera
+    },
+    {
+        id: 2,
+        heading: "Samyang 24-70 mm f/2,8 FE",
+        image: imgLens2470
+    },
+    {
+        id: 3,
+        heading: "Sony FE 14 mm f/1,8 GM",
+        image: imgLens14
+    },
+    {
+        id: 4,
+        heading: "Sony FE 85 mm f/1,8",
+        image: imgLens80
+    },
+    {
+        id: 5,
+        heading: "Sony FE 70-200 mm f/2,8 G OSS",
+        image: imgLens70200
+    },
+    {
+        id: 6,
+        heading: "Benro MeFoto Roadtrip",
+        image: imgTripod
+    },
+    {
+        id: 7,
+        heading: "Shimoda Explore V2 35",
+        img: imgBag
+    }
+]
