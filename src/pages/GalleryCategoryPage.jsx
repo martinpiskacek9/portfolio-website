47d9e4f11857d35ea7ae37bfeca6505bc4e397d2
martinpiskacek9 <martin.piskacek9@gmail.com>
2025-12-22 00:05:39 +0100

    galerryCategoryPage - heroImg podle kategorie fotek

--- src/pages/GalleryCategoryPage.jsx
+++ src/pages/GalleryCategoryPage.jsx
@@ -1,11 +1,15 @@
 import { useParams } from "react-router-dom";
-import React, { useMemo } from "react";
+import { useMemo } from "react";
+import imgAutomotive from "../imgs/automotive.webp";
+import imgHorses from "../imgs/horses.webp";
+import imgLandscapes from "../imgs/landscapes.webp";
+import imgTravel from "../imgs/travel.webp";
 
 const CATEGORY_META = {
-  landscapes: { title: "landscapes" },
-  automotive: { title: "automotive" },
-  travel: { title: "travel" },
-  horseserá: { title: "horses" },
+  landscapes: { title: "landscapes", heroImg: imgLandscapes },
+  automotive: { title: "automotive", heroImg: imgAutomotive },
+  travel: { title: "travel", heroImg: imgTravel},
+  horses: { title: "horses", heroImg: imgHorses },
 };
 
 export default function GalleryCategoryPage() {
@@ -41,25 +45,30 @@ export default function GalleryCategoryPage() {
 
   if (!meta) {
     return (
-      <section className="p-6">
+      <section className="flex items-center justify-center w-full h-100">
           <h1 className="text-2xl font-bold uppercase">error</h1>
       </section>
     );
   }
 
   return (
-    <section className="w-full py-12 px-6 md:px-16 2xl:px-28">
-      <h1 className="text-3xl font-black tracking-wide uppercase">{meta.title}</h1>
+    <section className="">
+      <div
+        className="w-full py-12 px-6 md:px-16 2xl:px-28 h-80 md:h-100 flex items-center justify-center flex-col bg-cover bg-center bg-no-repeat"
+        style={{ backgroundImage: `linear-gradient(rgba(0,0,0,0.6), rgba(0,0,0,0.4)), url(${meta.heroImg})` }}
+        >
+          <h1 className="uppercase text-4xl md:text-5xl font-montserrat font-black tracking-wide italic">{meta.title}</h1>
+      </div>
+
 
       {thumbsForCategory.length === 0 ? (
         <p className="mt-6 text-neutral-400">
-          Nenašel jsem žádné thumbnails v{" "}
-          <span className="font-mono">src/assets/imgs/thumbs/{category}</span>
+          ERROR
         </p>
       ) : (
-        <div className="mt-8 grid grid-cols-1 md:grid-cols-3 gap-6">
+        <div className="w-full h-full py-6 md:py-26 px-6 md:px-12 flex-col gap-2 grid grid-cols-1 md:grid-cols-3">
           {cols.map((col, colIndex) => (
-            <div key={colIndex} className="flex flex-col gap-6">
+            <div key={colIndex} className="flex flex-col gap-6 md:gap-2">
               {col.map((img) => (
                 <img
                   key={img.path}
@@ -67,7 +76,7 @@ export default function GalleryCategoryPage() {
                   alt={img.name}
                   loading="lazy"
                   decoding="async"
-                  className="w-full h-auto rounded-xl"
+                  className="w-full h-auto"
                 />
               ))}
             </div>
