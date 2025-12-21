c4117371b39bec10da8ac03bfeec4a2dffee8ebb
martinpiskacek9 <martin.piskacek9@gmail.com>
2025-12-21 22:19:35 +0100

    Úprava Gallery

--- src/components/PageDivider.jsx
+++ src/components/PageDivider.jsx
@@ -7,7 +7,7 @@ const PageDivider = () => {
         className="w-full h-full sm:py-0 sm:h-75 lg:h-100 bg-cover bg-center bg-no-repeat flex flex-col sm:flex-row"
         style={{ backgroundImage: `linear-gradient(rgba(0,0,0,0.1), rgba(0,0,0,0.9)), url("imgs/_DSC5754.webp")` }}
         >
-          <PageDividerButton side={"left"} btnLink={"/galleries"} btnHeading={"Gallery"} btnText={"Check out my work"} />
+          <PageDividerButton side={"left"} btnLink={"/gallery"} btnHeading={"Gallery"} btnText={"Check out my work"} />
           <PageDividerButton side={"right"} btnLink={"/gear"} btnHeading={"My Gear"} btnText={"What I shoot with"} />
         </div>
     </section>
--- src/pages/Gallery.jsx
+++ src/pages/Gallery.jsx
@@ -1,3 +1,6 @@
+import GalleryCategory from "../components/GalleryCategory"
+import { heroImages } from "../../images-config";
+
 const Gallery = () => {
   return (
     <section>
@@ -5,18 +8,18 @@ const Gallery = () => {
         className="w-full h-100 flex items-center justify-center flex-col bg-cover bg-center bg-no-repeat"
         style={{ backgroundImage: "linear-gradient(rgba(0,0,0,0.6), rgba(0,0,0,0.4)), url(imgs/_DSC7881.webp)" }}
         >
-          <h1 className="text-5xl font-montserrat font-black tracking-wide italic">GALLERY</h1>
+            <h1 className="text-5xl font-montserrat font-black tracking-wide italic">GALLERY</h1>
         </div>
 
-        <div className="w-full h-full py-26 px-6 md:px-26 2xl:px-36 flex gap-6">
-            <div className="flex-1 h-full border flex flex-col gap-6">
-
+        <div className="w-full h-full py-26 px-6 md:px-26 2xl:px-36 flex flex-col gap-6 items-center justify-center">
+            <div className="flex gap-6">
+                <GalleryCategory heading={heroImages[0].heading} img={heroImages[0].image} />
+                <GalleryCategory heading={heroImages[1].heading} img={heroImages[1].image} />
             </div>
-            <div className="flex-1 h-full flex flex-col border gap-6">
 
-            </div>
-            <div className="flex-1 h-full flex flex-col border gap-6">
-                
+            <div className="flex gap-6">
+                <GalleryCategory heading={heroImages[2].heading} img={heroImages[2].image} />
+                <GalleryCategory heading={heroImages[3].heading} img={heroImages[3].image} />
             </div>
         </div>
     </section>
