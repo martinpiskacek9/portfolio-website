c4c0d6dded035c1bfe3c81667c7a2f6f2b346ff8
martinpiskacek9 <martin.piskacek9@gmail.com>
2025-12-21 22:51:08 +0100

    GalleryCategory - změna z <a> na NavLink

--- src/components/GalleryCategory.jsx
+++ src/components/GalleryCategory.jsx
@@ -1,3 +1,4 @@
+import { NavLink } from "react-router-dom";
 
 const GalleryCategory = ({ heading, img, className = "" }) => {
   return (
@@ -6,10 +7,10 @@ const GalleryCategory = ({ heading, img, className = "" }) => {
       className="w-full h-full flex bg-center bg-no-repeat bg-cover hover:scale-105 duration-300 hover:duration-300 outline-neutral-300 hover:outline"
       style={{ backgroundImage: `linear-gradient(rgba(0,0,0,0.2), rgba(0,0,0,0.2)), url(${img})` }}
       >
-        <a href="#" className="font-montserrat w-full h-full flex items-center justify-center md:opacity-0 opacity-100 text-2xl md:text-xl uppercase font-bold hover:opacity-100 bg-black/50 hover:bg-black/80 hover:text-neutral-50 duration-300"
+        <NavLink to="#" className="font-montserrat w-full h-full flex items-center justify-center md:opacity-0 opacity-100 text-2xl md:text-xl uppercase font-bold hover:opacity-100 bg-black/50 hover:bg-black/80 hover:text-neutral-50 duration-300"
         >
           {heading}
-        </a>
+        </NavLink>
       </div>
     </div>
   )
