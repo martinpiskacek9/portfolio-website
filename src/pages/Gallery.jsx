71d5cee37c8337a1d0375350dd8df6f26da5f03b
martinpiskacek9 <martin.piskacek9@gmail.com>
2025-12-21 22:45:24 +0100

    doladění responzivity

--- src/pages/Gallery.jsx
+++ src/pages/Gallery.jsx
@@ -5,13 +5,13 @@ const Gallery = () => {
   return (
     <section>
         <div
-        className="w-full h-100 flex items-center justify-center flex-col bg-cover bg-center bg-no-repeat"
+        className="w-full h-80 md:h-100 flex items-center justify-center flex-col bg-cover bg-center bg-no-repeat"
         style={{ backgroundImage: "linear-gradient(rgba(0,0,0,0.6), rgba(0,0,0,0.4)), url(imgs/_DSC7881.webp)" }}
         >
-            <h1 className="text-5xl font-montserrat font-black tracking-wide italic">GALLERY</h1>
+            <h1 className="text-4xl md:text-5xl font-montserrat font-black tracking-wide italic">GALLERY</h1>
         </div>
 
-        <div className="w-full h-full py-26 px-6 md:px-26 2xl:px-36 flex flex-col gap-6 items-center justify-center">
+        <div className="w-full h-full py-6 md:py-26 px-6 md:px-26 2xl:px-36 flex flex-col gap-6 items-center justify-center">
             <div className="flex w-full h-140 md:h-full gap-6 md:flex-row flex-col">
                 <GalleryCategory heading={heroImages[0].heading} img={heroImages[0].image} className="flex-1" />
                 <GalleryCategory heading={heroImages[1].heading} img={heroImages[1].image} className="flex-1" />
